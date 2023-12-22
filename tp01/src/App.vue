25714ddcde3362d4d646f57be9bc0da69cb84a9d
HENG-Menghy <hengmenghy2002@gmail.com>
2023-12-22 18:11:07 +0700

    Add tp01 folder project

---
+++ tp01/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="wrapper">
+        <category-component/>
+        <promotion-component/>
+    </div>
+</template>
+
+<script></script>
+
+<style scoped>
+    .wrapper {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        align-items: center;
+        margin: 30px;
+    }
+</style>
